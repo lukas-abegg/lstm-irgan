296830317b7ea6bed0c28382ade903b383fd1ef6
Lukas Abegg <lukas.abegg1@gmail.com>
2018-12-08 11:59:51 +0100

    change loss
    Signed-off-by: Lukas Abegg <lukas.abegg1@gmail.com>

--- app/gan/adverserial_nn/generator.py
+++ app/gan/adverserial_nn/generator.py
@@ -20,64 +20,61 @@ class Generator:
         self.embeddings_layer_d: Embedding = embedding_layer_d
         self.reward = Input(shape=(None,), name='input_reward')
         self.important_sampling = Input(shape=(None,), name='input_imp_sampling')
-        self.model: Model = self.__get_model(model)
         self.sess = sess
+        self.__get_model(model)
 
     def __get_model(self, model):
         if model is None:
-            return self.__init_model()
+            self.__init_model()
         else:
-            return model
+            self.model = model
 
     def __init_model(self):
         # create model
 
-        sequence_input_q = Input(shape=(params.MAX_SEQUENCE_LENGTH,), dtype='int32', name='input_query')
-        embedded_sequences_q = self.embeddings_layer_q(sequence_input_q)
+        self.sequence_input_q = Input(shape=(params.MAX_SEQUENCE_LENGTH,), dtype='int32', name='input_query')
+        self.embedded_sequences_q = self.embeddings_layer_q(self.sequence_input_q)
 
-        lstm_q_in = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=True, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(embedded_sequences_q)
+        self.lstm_q_in = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=True, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(self.embedded_sequences_q)
         # this LSTM will transform the vector sequence into a single vector,
         # containing information about the entire sequence
-        lstm_q_out = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=False, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(lstm_q_in)
+        self.lstm_q_out = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=False, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(self.lstm_q_in)
 
-        sequence_input_d = Input(shape=(params.MAX_SEQUENCE_LENGTH,), dtype='int32', name='input_doc')
-        embedded_sequences_d = self.embeddings_layer_d(sequence_input_d)
+        self.sequence_input_d = Input(shape=(params.MAX_SEQUENCE_LENGTH,), dtype='int32', name='input_doc')
+        self.embedded_sequences_d = self.embeddings_layer_d(self.sequence_input_d)
 
-        lstm_d_in = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=True, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(embedded_sequences_d)
+        self.lstm_d_in = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=True, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(self.embedded_sequences_d)
         # this LSTM will transform the vector sequence into a single vector,
         # containing information about the entire sequence
-        lstm_d_out = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=False, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(lstm_d_in)
+        self.lstm_d_out = Bidirectional(GRU(params.GEN_HIDDEN_SIZE_LSTM, return_sequences=False, activation='elu', dropout=self.dropout, recurrent_dropout=self.dropout))(self.lstm_d_in)
 
-        x = Concatenate()([lstm_q_out, lstm_d_out])
-        x = Dropout(self.dropout)(x)
+        self.x = Concatenate()([self.lstm_q_out, self.lstm_d_out])
+        self.x = Dropout(self.dropout)(self.x)
 
         # we stack a deep fully-connected network on top
-        x = Dense(params.GEN_HIDDEN_SIZE_DENSE,
-                  activation='elu')(x)
-        x = Dense(1, activation='elu')(x)
+        self.x = Dense(params.GEN_HIDDEN_SIZE_DENSE,
+                  activation='elu')(self.x)
+        self.x = Dense(1, activation='elu')(self.x)
 
         # 0.2 should be replaced by self.temperature
-        score = Lambda(lambda z: z / 0.2, name='raw_score')(x)
-        score = Reshape([-1], name='score')(score)
-        prob = Activation('softmax', name='prob')(score)
+        self.score = Lambda(lambda z: z / 0.2, name='raw_score')(self.x)
+        self.score = Reshape([-1], name='score')(self.score)
+        self.prob = Activation('softmax', name='prob')(self.score)
 
-        model = Model(inputs=[sequence_input_q, sequence_input_d, self.reward, self.important_sampling], outputs=[prob])
-        model.summary()
+        self.model = Model(inputs=[self.sequence_input_q, self.sequence_input_d, self.reward, self.important_sampling], outputs=[self.prob])
+        self.model.summary()
 
-        adamw = AdamW(batch_size=params.GEN_BATCH_SIZE, samples_per_epoch=self.samples_per_epoch, epochs=params.GEN_TRAIN_EPOCHS)
+        self.adamw = AdamW(batch_size=params.GEN_BATCH_SIZE, samples_per_epoch=self.samples_per_epoch, epochs=params.GEN_TRAIN_EPOCHS)
 
-        model.compile(loss=self.loss(self.reward, self.important_sampling),
-                      optimizer=adamw,
+        self.model.compile(loss=self.loss(self.reward, self.important_sampling),
+                      optimizer=self.adamw,
                       metrics=[self.loss_metrics(self.reward, self.important_sampling)])
-
-        return model
 
     def loss(self, _reward, _important_sampling):
         def _loss(y_true, y_pred):
             log_action_prob = K.log(y_pred)
             loss = - K.reshape(log_action_prob, [-1]) * K.reshape(_reward, [-1]) * K.reshape(_important_sampling, [-1])
             loss = K.mean(loss)
-            print("Calculate loss: reward=", str(K.reshape(_reward, [-1])), " important sampling=", str(K.reshape(_important_sampling, [-1])), "loss=", loss)
             return loss
 
         return _loss
